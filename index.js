cdd77bc68894589c32824a72f647bcd54b11e9a8
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-05-27 22:59:17 -0300

    Change main file to use dependency injection

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 require('dotenv').config()
-var db = require('./src/database')
-var userController = require('./src/controller/users')
-var bodyParser = require('body-parser')
-var express = require('express')
-var app = express()
+const FirebaseDatabase = require('./src/database/firebase')
+const firebaseConfig = require('./src/database/firebaseConfig')
+const UserController = require('./src/controller/users')
+const UserDAO = require('./src/dao/users')
+const bodyParser = require('body-parser')
+const express = require('express')
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+const main = async () => {
+  try {
+    // === Conexão com o banco de dados (firebase) ===
+    const firebaseDatabase = new FirebaseDatabase()
+    const db = firebaseDatabase.getDatabaseConnection(firebaseConfig)
 
-// Routes
-app.get('/', userController.getUsers(db))
-app.get('/:id', userController.getUser(db))
-app.post('/:id', userController.createUser(db))
-app.put('/:id', userController.updateUser(db))
-app.delete('/:id', userController.deleteUser(db))
+    // === Instancias do DAO ===
+    const userDAO = new UserDAO(db)
 
-// Init Server
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+    // === Instancias do Controller ===
+    const userController = new UserController(userDAO)
 
-// app.get('/', (req, res) => res.status().json())
+    // === Configuração do express ===
+    const app = express()
+
+    // Middleware
+    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(bodyParser.json())
+
+    // Routes
+    app.get('/', userController.getUsers)
+    app.get('/:id', userController.getUser)
+    app.post('/:id', userController.createUser)
+    app.put('/:id', userController.updateUser)
+    app.delete('/:id', userController.deleteUser)
+
+    // Init Server
+    app.listen(3000, () => console.log('Example app listening on port 3000!'))
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+main()
