9447dc774a2869095578d562bbdebb908a0d0a1b
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-05-31 02:29:07 -0300

    Refactor UserDAO

--- src/dao/users.js
+++ src/dao/users.js
@@ -1,34 +1,37 @@
+const { ErrorHandler } = require('../middleware/error')
+
 class UserDAO {
   constructor(db) {
     this.db = db.collection('users')
   }
 
-  getUsers = async () => {
-    try {
-      const snapshot = await this.db.get()
-      const usersData = snapshot.docs.map(doc => doc.data())
-      return usersData
-    } catch (e) {
-      console.error(e)
-    }
+  async getUsers(filter) {
+    const snapshot = await this.db.limit(filter.limit || 10).get()
+    const usersData = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+    return usersData
   }
 
-  getUser = async (userID) => {
-    const snapshot = await this.db.doc(userID).get()
+  async getUser(userInput) {
+    const snapshot = await this.db.doc(userInput.id).get()
+    if (!snapshot.exists) throw new ErrorHandler(401, 'Usuário não foi encontrado')
     const userData = snapshot.data()
     return userData
   }
 
-  createUser = async (user) => {
-    // TODO: codar o createUser
+  async createUser(user) {
+    const { id } = await this.db.add(user)
+    return { id, ...user }
   }
 
-  updateUser = async (user) => {
-    // TODO: codar o updateUser
+  async updateUser(user) {
+    const { id, ...rest } = user
+    await this.db.doc(id).update(rest)
+    return user
   }
 
-  deleteUser = async (user) => {
-    // TODO: codar o deleteUser
+  async deleteUser(user) {
+    await this.db.doc(user.id).delete()
+    return user
   }
 }
 
